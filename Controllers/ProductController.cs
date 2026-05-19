e39535550643959afb1d7f970acdbff3e8f0443a
Bya Sebastien <byaebastien@hotmail.com>
2026-05-19 15:38:07 +0200

    Debug

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -13,89 +13,89 @@ namespace TI_Devops_2026_Bend_IAC.Controllers
     [ApiController]
     [Route("api/[controller]")]
     public class ProductController(
-        //MyDbContext context,
-        //BlobContainerClient container,
-        //ServiceBusClient bus,
+        MyDbContext context,
+        BlobContainerClient container,
+        ServiceBusClient bus,
         IConfiguration configuration
         ) : ControllerBase
     {
 
-        //[HttpPost]
-        //public async Task<IActionResult> AddAsync([FromForm] ProductForm request)
-        //{
-        //    try
-        //    {
+        [HttpPost]
+        public async Task<IActionResult> AddAsync([FromForm] ProductForm request)
+        {
+            try
+            {
 
-        //        var p = new Product { Name = request.Name };
-        //        var added = context.Add(p).Entity;
-        //        await context.SaveChangesAsync();
-        //        //var added = new Product { Name = request.Name, Id = 42 };
+                var p = new Product { Name = request.Name };
+                var added = context.Add(p).Entity;
+                await context.SaveChangesAsync();
+                //var added = new Product { Name = request.Name, Id = 42 };
 
-        //        var stream = new MemoryStream();
-        //        await request.Image.CopyToAsync(stream);
-        //        stream.Position = 0;
+                var stream = new MemoryStream();
+                await request.Image.CopyToAsync(stream);
+                stream.Position = 0;
 
-        //        var blobName = Guid.NewGuid().ToString();
-        //        var blobClient = container.GetBlobClient(blobName);
+                var blobName = Guid.NewGuid().ToString();
+                var blobClient = container.GetBlobClient(blobName);
 
-        //        var uploadOptions = new BlobUploadOptions
-        //        {
-        //            HttpHeaders = new BlobHttpHeaders
-        //            {
-        //                ContentType = request.Image.ContentType
-        //            },
-        //            Metadata = new Dictionary<string, string>
-        //        {
-        //            {
-        //                "productId", added.Id.ToString()
-        //            },
-        //        }
-        //        };
+                var uploadOptions = new BlobUploadOptions
+                {
+                    HttpHeaders = new BlobHttpHeaders
+                    {
+                        ContentType = request.Image.ContentType
+                    },
+                    Metadata = new Dictionary<string, string>
+                {
+                    {
+                        "productId", added.Id.ToString()
+                    },
+                }
+                };
 
-        //        await blobClient.UploadAsync(stream, uploadOptions);
+                await blobClient.UploadAsync(stream, uploadOptions);
 
-        //        var sender = bus.CreateSender("devops-2026-queue");
+                var sender = bus.CreateSender("devops-2026-queue");
 
-        //        var message = new ServiceBusMessage(
-        //                JsonSerializer.Serialize(added)
-        //            );
+                var message = new ServiceBusMessage(
+                        JsonSerializer.Serialize(added)
+                    );
 
-        //        await sender.SendMessageAsync(message);
+                await sender.SendMessageAsync(message);
 
-        //        return Accepted(added);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
+                return Accepted(added);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
-        //[HttpGet("image/{productId}")]
-        //public async Task<IActionResult> GetImage([FromRoute] string productId)
-        //{
-        //    try
-        //    {
-        //        var blob = await container.GetBlobsAsync().FirstOrDefaultAsync(b =>
-        //        {
-        //            var blobClient = container.GetBlobClient(b.Name);
-        //            var properties = blobClient.GetPropertiesAsync().ConfigureAwait(false).GetAwaiter().GetResult();
-        //            return properties.Value.Metadata.TryGetValue("productId", out var id) && id == productId;
-        //        });
+        [HttpGet("image/{productId}")]
+        public async Task<IActionResult> GetImage([FromRoute] string productId)
+        {
+            try
+            {
+                var blob = await container.GetBlobsAsync().FirstOrDefaultAsync(b =>
+                {
+                    var blobClient = container.GetBlobClient(b.Name);
+                    var properties = blobClient.GetPropertiesAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+                    return properties.Value.Metadata.TryGetValue("productId", out var id) && id == productId;
+                });
 
-        //        if (blob is null) return NotFound();
+                if (blob is null) return NotFound();
 
-        //        var blobClient = container.GetBlobClient(blob.Name);
-        //        var properties = await blobClient.GetPropertiesAsync();
-        //        var contentType = properties.Value.ContentType ?? "application/octet-stream";
-        //        var stream = await blobClient.OpenReadAsync();
+                var blobClient = container.GetBlobClient(blob.Name);
+                var properties = await blobClient.GetPropertiesAsync();
+                var contentType = properties.Value.ContentType ?? "application/octet-stream";
+                var stream = await blobClient.OpenReadAsync();
 
-        //        return File(stream, contentType, blob.Name);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-        //}
+                return File(stream, contentType, blob.Name);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
         [HttpGet("connectionString")]
         public async Task<IActionResult> GetConnectionString()
@@ -106,7 +106,7 @@ namespace TI_Devops_2026_Bend_IAC.Controllers
                 {
                     Main = configuration.GetConnectionString("Main"),
                     Blob = configuration.GetConnectionString("Blob"),
-                    ServiceBus = configuration.GetConnectionString("Bus")
+                    Bus = configuration.GetConnectionString("Bus")
                 };
 
                 return Ok(connectionStrings);
