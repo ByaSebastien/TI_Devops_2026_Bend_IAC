fa9c4bf2a987791caa05ba11b7570d94d9e3674e
Bya Sebastien <byaebastien@hotmail.com>
2026-05-19 15:11:06 +0200

    Test

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
 using TI_Devops_2026_Bend_IAC.contexts;
 using TI_Devops_2026_Bend_IAC.Dtos;
@@ -9,61 +10,59 @@ using TI_Devops_2026_Bend_IAC.Entities;
 
 namespace TI_Devops_2026_Bend_IAC.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class ProductController(
         MyDbContext context,
         BlobContainerClient container,
-        ServiceBusClient bus
+        ServiceBusClient bus,
+        IConfiguration configuration
         ) : ControllerBase
     {
 
         [HttpPost]
-        public async Task<IActionResult> AddAsync([FromForm] ProductForm form)
+        public async Task<IActionResult> AddAsync([FromForm] ProductForm request)
         {
             try
             {
 
-
-                var product = new Product
-                {
-                    Name = form.Name
-                };
-
-                var added = (await context.Products.AddAsync(product)).Entity;
+                var p = new Product { Name = request.Name };
+                var added = context.Add(p).Entity;
                 await context.SaveChangesAsync();
+                //var added = new Product { Name = request.Name, Id = 42 };
 
                 var stream = new MemoryStream();
-
-                await form.Image.CopyToAsync(stream);
-
+                await request.Image.CopyToAsync(stream);
                 stream.Position = 0;
 
                 var blobName = Guid.NewGuid().ToString();
-
                 var blobClient = container.GetBlobClient(blobName);
 
                 var uploadOptions = new BlobUploadOptions
                 {
                     HttpHeaders = new BlobHttpHeaders
                     {
-                        ContentType = form.Image.ContentType
+                        ContentType = request.Image.ContentType
                     },
                     Metadata = new Dictionary<string, string>
                 {
-                    { "productId", added.Id.ToString() }
+                    {
+                        "productId", added.Id.ToString()
+                    },
                 }
                 };
 
                 await blobClient.UploadAsync(stream, uploadOptions);
 
-                var sender = bus.CreateSender("devops-2026-queue");
+                var sender = bus.CreateSender("seb-servicebusqueue-for-test-bicep");
 
-                var message = new ServiceBusMessage(JsonSerializer.Serialize(added));
+                var message = new ServiceBusMessage(
+                        JsonSerializer.Serialize(added)
+                    );
 
                 await sender.SendMessageAsync(message);
 
-                return Ok(added);
+                return Accepted(added);
             }
             catch (Exception ex)
             {
@@ -74,24 +73,48 @@ namespace TI_Devops_2026_Bend_IAC.Controllers
         [HttpGet("image/{productId}")]
         public async Task<IActionResult> GetImage([FromRoute] string productId)
         {
-            var blob = await container.GetBlobsAsync().FirstOrDefaultAsync(b =>
+            try
             {
-                var blobClient = container.GetBlobClient(b.Name);
-                var properties = blobClient.GetPropertiesAsync().ConfigureAwait(false).GetAwaiter().GetResult();
-                return properties.Value.Metadata.TryGetValue("productId", out var id) && id.ToString() == productId;
-            });
+                var blob = await container.GetBlobsAsync().FirstOrDefaultAsync(b =>
+                {
+                    var blobClient = container.GetBlobClient(b.Name);
+                    var properties = blobClient.GetPropertiesAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+                    return properties.Value.Metadata.TryGetValue("productId", out var id) && id == productId;
+                });
 
-            if (blob == null)
-            {
-                return NotFound();
+                if (blob is null) return NotFound();
+
+                var blobClient = container.GetBlobClient(blob.Name);
+                var properties = await blobClient.GetPropertiesAsync();
+                var contentType = properties.Value.ContentType ?? "application/octet-stream";
+                var stream = await blobClient.OpenReadAsync();
+
+                return File(stream, contentType, blob.Name);
             }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
-            var blobClient = container.GetBlobClient(blob.Name);
-            var properties = await blobClient.GetPropertiesAsync();
-            var contentType = properties.Value.ContentType ?? "application/octet-stream";
-            var stream = await blobClient.OpenReadAsync();
+        [HttpGet("connectionString")]
+        public async Task<IActionResult> GetConnectionString()
+        {
+            try
+            {
+                var connectionStrings = new
+                {
+                    Main = configuration.GetConnectionString("Main"),
+                    Blob = configuration.GetConnectionString("Blob"),
+                    ServiceBus = configuration.GetConnectionString("Bus")
+                };
 
-            return File(stream, contentType, blob.Name);
+                return Ok(connectionStrings);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
